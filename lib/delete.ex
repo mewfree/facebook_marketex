3c3235378c69e04231b9fcc5aa48b29eed531aee
Damien Gonot <damien.gonot@gmail.com>
2017-09-27 23:37:13 -0400

    Add Delete node function

---
+++ lib/delete.ex
@@ -0,0 +1,13 @@
+defmodule FacebookMarketex.Delete do
+  use Tesla
+
+  plug Tesla.Middleware.BaseUrl, "https://graph.facebook.com"
+  plug Tesla.Middleware.JSON
+  plug Tesla.Middleware.Query, [access_token: System.get_env("FACEBOOK_ACCESS_TOKEN")]
+
+  adapter Tesla.Adapter.Hackney
+
+  def node(id) do
+    delete("/v2.10/" <> id).body
+  end
+end
