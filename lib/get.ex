f13475cb63710ea89ff9949cae5fc68a1f4707a0
Damien Gonot <damien.gonot@gmail.com>
2017-10-05 23:30:49 -0400

    Custom access_token for Get commands

--- lib/get.ex
+++ lib/get.ex
@@ -3,32 +3,32 @@ defmodule FacebookMarketex.Get do
 
   plug Tesla.Middleware.BaseUrl, "https://graph.facebook.com"
   plug Tesla.Middleware.JSON
-  plug Tesla.Middleware.Query, [access_token: System.get_env("FACEBOOK_ACCESS_TOKEN")]
 
   adapter Tesla.Adapter.Hackney
 
-  def ad_accounts(fields \\ []) do
-    get("/v2.10/me/adaccounts", query: [fields: Enum.join(fields, ",")]).body
+  def ad_accounts(access_token, fields \\ []) do
+    get("/v2.10/me/adaccounts", query: [access_token: access_token, fields: Enum.join(fields, ",")]).body
     |> Map.get("data")
   end
 
-  def data(id, fields) do
-    get("/v2.10/" <> id, query: [fields: Enum.join(fields, ",")]).body
+  def data(access_token, id, fields) do
+    get("/v2.10/" <> id, query: [access_token: access_token, fields: Enum.join(fields, ",")]).body
   end
 
-  def name(id) do
-    data(id, ["name"])
+  def name(access_token, id) do
+    data(access_token, id, ["name"])
     |> Map.get("name")
   end
 
-  def campaigns(account_id, fields \\ []) do
-    get("/v2.10/" <> account_id <> "/campaigns", query: [fields: Enum.join(fields, ",")]).body
+  def campaigns(access_token, account_id, fields \\ []) do
+    get("/v2.10/" <> account_id <> "/campaigns", query: [access_token: access_token, fields: Enum.join(fields, ",")]).body
     |> Map.get("data")
   end
 
-  def insights(id, fields, from, to, level \\ "", breakdowns \\ []) do
+  def insights(access_token, id, fields, from, to, level \\ "", breakdowns \\ []) do
     get("/v2.10/" <> id <> "/insights",
         query: [
+          access_token: access_token,
           fields: Enum.join(fields, ","),
           time_range: Poison.encode!(%{"since": from, "until": to}),
           breakdowns: breakdowns,
